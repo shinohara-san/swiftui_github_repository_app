d1858b1c47941d4df4a685090b1259ef60327ee6
shinohara-san <someiyoshino0809@gmail.com>
2022-04-17 18:27:53 +0900

    Initial Commit

---
+++ GithubRepo/GithubRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubRepoApp.swift
+//  GithubRepo
+//
+//  Created by Yuki Shinohara on 2022/04/17.
+//
+
+import SwiftUI
+
+@main
+struct GithubRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
