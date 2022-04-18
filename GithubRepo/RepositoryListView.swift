4c9be56641aef45547b0a5f18c59645b2e3dec5f
shinohara-san <someiyoshino0809@gmail.com>
2022-04-18 20:31:05 +0900

    webviewをsheetで表示

--- GithubRepo/RepositoryListView.swift
+++ GithubRepo/RepositoryListView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct RepositoryListView: View {
 
     let repositories: [Repository]?
+    @State private var showingSheet = false
 
     var body: some View {
         List {
             ForEach(repositories?.filter { !$0.fork } ?? [Repository]() ) { repo in
-                NavigationLink {
-                    RepositoryWebView(url: repo.url)
+                Button {
+                    self.showingSheet.toggle()
                 } label: {
                     VStack(alignment: .leading, spacing: 8) {
                         Text(repo.name)
@@ -28,8 +29,25 @@ struct RepositoryListView: View {
                         }
                     }
                 }
+                .foregroundColor(.black)
+                .sheet(isPresented: $showingSheet) {
+                    NavigationView {
+                        RepositoryWebView(url: repo.url)
+                            .navigationBarTitle(Text(repo.name),
+                                                displayMode: .inline)
+                            .toolbar {
+                                ToolbarItem(placement: .navigationBarLeading) {
+                                    Button("閉じる") {
+                                        showingSheet.toggle()
+                                    }
+                                    .foregroundColor(Color.white)
+                                }
+                            }
+                    }
+                }
             }
         }
+        .listStyle(.grouped)
     }
 }
 
